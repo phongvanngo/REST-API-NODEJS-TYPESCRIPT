24f9a93af35ca9dd1af2d3cf9837e82f839d7470
phongvanngo <19520834@gm.uit.edu.vn>
2022-04-23 05:30:37 +0700

    update docker compose

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,6 @@
 export default {
-  port: 1337,
-  dbUri: "mongodb://localhost:27017/rest-api-tutorial",
+  port: 3000,
+  dbUri: "mongodb://103.142.137.207:27017/rest-api-tutorial",
   saltWorkFactor: 10,
   accessTokenTtl: "15m",
   refreshTokenTtl: "1y",
